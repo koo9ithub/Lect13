8ffc94b831caa92e72e2180e658177086917a902
koo9ithub <kk223704@naver.com>
2018-11-29 16:06:52 +0900

    3rd submission

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 
 int fget_word(FILE *fp, char *word);
-int is_whitespace(char *word[int cnt]);
+int is_whitespace(char c);
 
 int main(int argc, char *argv[]) {
 	
@@ -24,9 +24,7 @@ int main(int argc, char *argv[]) {
 		return -1;		//0으로 해도 되나 0으로 끝나는 것과는 다른 결과를 의미하므로 -1 사용. 
 	}
 	
-	
-	printf("Input a word you want to find: ");
-	scanf("%s", word);
+
 	
 	while (fget_word(fp, word) != 0)
 	{
@@ -46,18 +44,19 @@ int fget_word(FILE *fp, char *word) {
 	
 	while ( (c=fgetc(fp)) != EOF )
 	{
-		if (is_whitespace(word) == 0)
+		if (is_whitespace(c) == 0) //유의미한 문자이면 나감 
 			break;
 	}
-	if (c == EOF)
+	if (c == EOF) //최초 읽은 문자가 마지막일 때 
 		return 0;
 	
+	//본격적으로 단어를 한 글자씩 읽음 
 	cnt = 0;
 	word[cnt++] = c;
 	word[cnt] = '\0';
 	
 	while ( (word[cnt]=fgetc(fp)) != EOF) {
-		if ( is_whitespace(word[cnt]) == 1 ) 
+		if ( is_whitespace(word[cnt]) == 1 ) //공백문자면 받은거 취소하고 나감 
 		{
 			word[cnt] = '\0';
 			break;
@@ -67,3 +66,17 @@ int fget_word(FILE *fp, char *word) {
 	
 	return cnt;	
 }
+
+int is_whitespace(char c) {
+
+	if (c == ' ' ||
+		c == '\t' ||
+		c == '\n' ||
+		c == '\r' ||
+		c == '(' ||
+		c == ')' ) 
+	return 1; //공백 글자다.
+	
+	return 0; //유의미한 글자다.	
+}
+
